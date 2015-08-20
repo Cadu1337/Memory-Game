2003c233a415eb9c2713cc5b8854f8f84380e494
Carlos Eduardo Farias Ribeiro <carlosribeiro@cejoseleitelopes.com.br>
2015-08-19 21:31:37 -0300

    Mecânica
    Mecânica do Jogo da Memória completa, restando fazer somente, menu, tela
    de vitória e sons do jogo.

---
+++ Memory-Game-master/Assets/Scripts/IDs.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class IDs : MonoBehaviour 
+{
+	public int Clicks;
+	public Image[] Numbers;
+	public MyID[] Cards;
+	public GameObject[] Cartas;
+	private int CounterTo;
+	private int MainCard;
+	public bool Trying;
+	private float j;	
+	private bool Right;
+	public float NonVired;
+	public bool CanDesvira;
+	public int Carta1;
+	public int Carta2;
+	public GameObject[] ActualCards;
+	private int countingstars;
+	void Start () 
+	{
+		Clicks = 0;
+		j = 0;
+		Trying = true;
+		MainCard = Mathf.FloorToInt(Random.Range(0,11));
+		CounterTo = 0;
+		for(int i = 0; i < 12;i++)
+		{
+			Cards[i] = GameObject.Find(""+(i + 1)).GetComponent<MyID>();
+		}	
+	}
+	void Update () 
+	{
+		if(Clicks == 2 && Carta1 != Carta2)
+		{
+			CanDesvira = true;
+		}
+		else if(Carta1 == Carta2 && Carta1 != 0 && Carta2 != 0)
+		{
+			ActualCards = GameObject.FindGameObjectsWithTag(Carta1+"");
+			ActualCards[0].GetComponent<MyID>().Certou = true;
+			ActualCards[1].GetComponent<MyID>().Certou = true;
+			Carta1 = 0;
+			Carta2 = 0;
+			Clicks = 0;
+		}
+		if(CanDesvira)
+		{
+			NonVired += Time.deltaTime;
+		}
+		if(NonVired > 2f)
+		{
+			for(int i = 0; i < Cards.Length; i++)
+			{
+				Carta1 = 0;
+				Carta2 = 0;
+				NonVired = 0;
+				CanDesvira = false;
+				Clicks = 0;
+				if(Cards[i].Certou == false)
+				{
+					Cards[i].Isvired = false;
+				}
+			}
+		}
+		if(j > 3f)
+		{
+			Trying = false;
+		}
+		else
+		{
+			j += Time.deltaTime;
+		}
+		if(Trying)
+		{
+			if(CounterTo < 2 && Cards[MainCard].MyCard == 0)
+			{
+				Cards[MainCard].MyCard = 1;
+				Cartas[MainCard].tag = 1+"";
+				CounterTo += 1;	
+			}
+			else if(CounterTo < 4 && Cards[MainCard].MyCard == 0)
+			{
+				Cards[MainCard].MyCard = 2;
+				Cartas[MainCard].tag = 2+"";
+				CounterTo += 1;	
+			}
+			else if(CounterTo < 6 && Cards[MainCard].MyCard == 0)
+			{
+				Cards[MainCard].MyCard = 3;
+				Cartas[MainCard].tag = 3+"";
+				CounterTo += 1;	
+			}
+			else if(CounterTo < 8 && Cards[MainCard].MyCard == 0)
+			{
+				Cards[MainCard].MyCard = 4;
+				Cartas[MainCard].tag = 4+"";
+				CounterTo += 1;	
+			}
+			else if(CounterTo < 10 && Cards[MainCard].MyCard == 0)
+			{
+				Cards[MainCard].MyCard = 5;
+				Cartas[MainCard].tag = 5+"";
+				CounterTo += 1;	
+			}
+			else if(CounterTo < 12 && Cards[MainCard].MyCard == 0)
+			{
+				Cards[MainCard].MyCard = 6;
+				Cartas[MainCard].tag = 6+"";
+				CounterTo += 1;	
+			}
+			else
+			{
+				for(int i = 0; i < 12; i++)
+				{
+					if(Cards[i].MyCard == 0)
+					{
+						MainCard = Mathf.FloorToInt(Random.Range(0,12));
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Memory-Game-master/Assets/Scripts/MyID.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MyID : MonoBehaviour 
+{
+	private IDs Clickzin;
+	public int MyCard; 
+	public bool Isvired;
+	public Sprite[] IdImage;
+	public Sprite MyImage;
+	public bool Certou;
+	
+	void Start () 
+	{
+		Clickzin = GameObject.Find("Canvas").GetComponent<IDs>();
+		MyImage = gameObject.GetComponent<Image>().sprite;
+		for(int i = 0; i < 6; i++)
+		{
+			IdImage[i] = Resources.Load("Sprites/"+i,typeof(Sprite)) as Sprite;
+		}
+	}
+	
+	void Update () 
+	{
+		if(Isvired)
+		{
+			gameObject.GetComponent<Image>().sprite = MyImage;
+			switch(MyCard)
+			{
+				case 1:
+					MyImage = IdImage[0];
+				break;
+				case 2:
+					MyImage = IdImage[1];
+				break;
+				case 3:
+					MyImage = IdImage[2];
+				break;
+				case 4:
+					MyImage = IdImage[3];
+				break;
+				case 5:
+					MyImage = IdImage[4];
+				break;
+				case 6:
+					MyImage = IdImage[5];
+				break;
+			}
+		}
+		else
+		{
+			gameObject.GetComponent<Image>().sprite = Resources.Load("Sprites/Default",typeof(Sprite)) as Sprite;
+		}
+	}
+	public void OnClicked()
+	{
+		if(Clickzin.Clicks < 2)
+		{
+			Clickzin.Clicks += 1;
+			if(Clickzin.Carta1 == 0 || Clickzin.Carta1 == null)
+			{
+				Clickzin.Carta1 = MyCard;
+			}
+			else if(Clickzin.Carta2 == 0 || Clickzin.Carta1 == null)
+			{
+				Clickzin.Carta2 = MyCard;
+			}
+			Isvired = true;
+		}
+	}
+}
